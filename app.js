8ed30c1d13942be04aa422622e24dfebaba2f268
JulijaA <44105016+JulijaA@users.noreply.github.com>
2021-11-22 10:13:09 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+$.ajax({
+  url: "https://geolocation-db.com/jsonp",
+  jsonpCallback: "callback",
+  dataType: "jsonp",
+  success: function(location) {
+    $('#country').html(location.country_name);
+  }
+});
